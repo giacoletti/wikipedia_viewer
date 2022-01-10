bf020ca6ae0d888402aa52c7b01dbdc35517c72b
giacoletti <giacoletti92@gmail.com>
2022-01-10 22:56:39 +0100

    Adds mui Container with random article button and search article input in App.jsx adds data-cy attributes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Container, Fab, InputBase } from '@mui/material';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import SearchIcon from '@mui/icons-material/Search';
 
 const App = () => {
+
   return (
-    <h1 data-cy="wiki-viewer-header">Wikipedia Search</h1>
+    <Container>
+      <h1 data-cy="wiki-viewer-header">Wikipedia Search</h1>
+      <Fab data-cy="random-article-btn" color="secondary" sx={{ m: 1 }}>
+        <ShuffleIcon />
+      </Fab>
+      <Fab data-cy="search-article-btn" color="primary" 
+        variant="extended" sx={{ m: 1 }}>
+        <SearchIcon />
+        <InputBase placeholder="Search" sx={{ m: 1, color: 'white' }} />
+      </Fab>
+    </Container>
   );
 };
 
